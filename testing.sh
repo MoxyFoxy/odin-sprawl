b65642f5c9d9fbdaef0b3ce91ac760bd63c9f37e
F0x1fy <alecsanchez@avian-lang.org>
2020-11-04 20:19:47 -0700

    Fixed typo.

--- testing.sh
+++ testing.sh
@@ -4,5 +4,5 @@ odin build . -define:CCOMPAT=true -build-mode:shared -out:bin/libsprawlc -keep-t
 ld bin/libsprawlc.o -shared -o bin/libsprawlc.so -init "___\$shared_runtime"
 sudo mv bin/libsprawlc.so /usr/local/lib
 cd c
-gcc -L. test.c sprawlc.c -lsprawl
+gcc -L. test.c sprawlc.c -lsprawlc
 ./a.out
