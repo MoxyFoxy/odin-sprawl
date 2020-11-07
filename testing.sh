5d191f56e524d1f7da04704cc4bf578e0b52f91c
F0x1fy <alecsanchez@avian-lang.org>
2020-11-06 22:48:41 -0700

    Forgot to remove the testing script.

--- testing.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-odin build . -define:CCOMPAT=true -build-mode:shared -out:bin/libsprawlc -keep-temp-files
-ld bin/libsprawlc.o -shared -o bin/libsprawlc.so -init "__\$startup_runtime"
-sudo mv bin/libsprawlc.so /usr/local/lib
-cd c
-gcc -L. test.c sprawlc.c -lsprawlc
-./a.out
